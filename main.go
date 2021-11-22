4c3987ac809b3b9bc59c3ca8e5d9043bb9e07ce0
Kosachev Daniil <kos-dan@mail.ru>
2021-11-22 10:16:53 +0300

    Remade with signle module

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"gameOfLife/gameoflife"
-)
-
 func main() {
-	// game, err := gameoflife.ReadFromFileWithInt("./grids/glider")
+	// game, err := ReadFromFileWithInt("./grids/glider")
 	// if err != nil {
 	// 	fmt.Println(err)
 	// 	return
 	// }
-	game := gameoflife.CreateRandom(40, 20, 200)
+	game := CreateRandom(40, 20, 200)
 	game.RunWithConsole(200)
 }
